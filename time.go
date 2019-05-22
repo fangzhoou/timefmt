02c7b8c8abe8000bd526fff099d5b8324b53c5cb
lxw <fangzhoou@gmail.com>
2019-05-22 22:57:48 +0800

    init

---
+++ time.go
@@ -0,0 +1,14 @@
+package timefmt
+
+const (
+
+	golangBornTime = "2006-01-02 15:04:05"
+)
+
+
+// 获取当前日期时间，datetime 格式：YYYY-mm-dd H:i:s
+// 如：2019-05-22 22:36:20
+func Datetime() string {
+	return golangBornTime
+}
+
